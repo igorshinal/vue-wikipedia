3fc22c8600934eff5b5055c034bd5784f65b05aa
igorshinal <igorshinal@gmail.com>
2018-01-28 22:01:14 +0200

    Vue wikipedia app

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+   <header>
+     <div class="logo">
+       <img src="./assets/logo.png" alt="">
+     </div>
+     <search></search>
+   </header>
+    <main>
+      <results></results>
+    </main>
+  </div>
+</template>
+
+<script>
+  import Search from './components/Search'
+  import Results from './components/Results'
+export default {
+  name: 'app',
+  components: {
+    Search,
+    Results
+  }
+}
+</script>
+
+<style src="./assets/style.css"></style>
---
+++ src/components/Search.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="Search">
+    <input type="text" class="Search__input" v-model="query" v-on:keyup.13="search">
+    <button class="Search__button" @click="search">Найти</button>
+  </div>
+
+</template>
+
+<script>
+    export default {
+      data() {
+        return {
+          query: ''
+        }
+      },
+      methods: {
+        search() {
+          this.$store.dispatch('search', this.query)
+        }
+      }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
